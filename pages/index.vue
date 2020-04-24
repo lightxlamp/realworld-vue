7202bd3711f7cd728a87b16c1a2713f599544f3f
lightxlamp <sheveliukhin.s@gmail.com>
2020-04-24 15:28:51 +0600

    Add some code from starter kit

--- pages/index.vue
+++ pages/index.vue
@@ -1,66 +1,85 @@
 <template>
-  <div class="container">
-    <div>
-      <logo />
-      <h1 class="title">
-        realworld-vue
-      </h1>
-      <h2 class="subtitle"></h2>
-      <div class="links">
-        <a href="https://nuxtjs.org/" target="_blank" class="button--green">
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
+    <div class="home-page">
+
+        <div class="banner">
+            <div class="container">
+            <h1 class="logo-font">conduit</h1>
+            <p>A place to share your knowledge.</p>
+            </div>
+        </div>
+
+        <div class="container page">
+            <div class="row">
+
+            <div class="col-md-9">
+                <div class="feed-toggle">
+                <ul class="nav nav-pills outline-active">
+                    <li class="nav-item">
+                    <a class="nav-link disabled" href="">Your Feed</a>
+                    </li>
+                    <li class="nav-item">
+                    <a class="nav-link active" href="">Global Feed</a>
+                    </li>
+                </ul>
+                </div>
+
+                <div class="article-preview">
+                <div class="article-meta">
+                    <a href="profile.html"><img src="http://i.imgur.com/Qr71crq.jpg" /></a>
+                    <div class="info">
+                    <a href="" class="author">Eric Simons</a>
+                    <span class="date">January 20th</span>
+                    </div>
+                    <button class="btn btn-outline-primary btn-sm pull-xs-right">
+                    <i class="ion-heart"></i> 29
+                    </button>
+                </div>
+                <a href="" class="preview-link">
+                    <h1>How to build webapps that scale</h1>
+                    <p>This is the description for the post.</p>
+                    <span>Read more...</span>
+                </a>
+                </div>
+
+                <div class="article-preview">
+                <div class="article-meta">
+                    <a href="profile.html"><img src="http://i.imgur.com/N4VcUeJ.jpg" /></a>
+                    <div class="info">
+                    <a href="" class="author">Albert Pai</a>
+                    <span class="date">January 20th</span>
+                    </div>
+                    <button class="btn btn-outline-primary btn-sm pull-xs-right">
+                    <i class="ion-heart"></i> 32
+                    </button>
+                </div>
+                <a href="" class="preview-link">
+                    <h1>The song you won't ever stop singing. No matter how hard you try.</h1>
+                    <p>This is the description for the post.</p>
+                    <span>Read more...</span>
+                </a>
+                </div>
+
+            </div>
+
+            <div class="col-md-3">
+                <div class="sidebar">
+                <p>Popular Tags</p>
+
+                <div class="tag-list">
+                    <a href="" class="tag-pill tag-default">programming</a>
+                    <a href="" class="tag-pill tag-default">javascript</a>
+                    <a href="" class="tag-pill tag-default">emberjs</a>
+                    <a href="" class="tag-pill tag-default">angularjs</a>
+                    <a href="" class="tag-pill tag-default">react</a>
+                    <a href="" class="tag-pill tag-default">mean</a>
+                    <a href="" class="tag-pill tag-default">node</a>
+                    <a href="" class="tag-pill tag-default">rails</a>
+                </div>
+                </div>
+            </div>
+
+            </div>
+        </div>
+        
     </div>
-  </div>
 </template>
-
-<script>
-import Logo from '~/components/Logo.vue'
-
-export default {
-  components: {
-    Logo
-  }
-}
-</script>
-
-<style>
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
-</style>
